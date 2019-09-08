836623c9fd12ac3e8c18a66bcfb446488c45014e
philippe <philippefanaro@gmail.com>
2019-09-07 23:16:34 -0300

    casos de uso básicos criados; fixes #2

--- zds_slack/slacky.py
+++ zds_slack/slacky.py
@@ -12,6 +12,12 @@ class CustomSlackBot():
     ):
         """
         Initializes a custom Slack bot.
+
+        Args:
+            slack_token (str) : security token to grant the bot access to the 
+                workspace. You can find it under 
+                [slack api](https://api.slack.com/apps/) inside the 
+                <kbd> Install App </kbd> > ``Bot User OAuth Access Token``.
         """
 
         self.slack_token = slack_token
@@ -26,26 +32,67 @@ class CustomSlackBot():
         self.RTMClient.start()
 
     @slack.RTMClient.run_on(event='message')
-    def say_hello(**payload):
+    def answer_message(**payload):
         """
-        Says hello to an user.
+        Answers a message sent to the channel.
         """
 
         data = payload['data']
         web_client = payload['web_client']
         rtm_client = payload['rtm_client']
+        channel_id = data['channel']
+        thread_ts = data['ts']
 
-        if (
-            'text' in data.keys() and 
-            'Hello' in data['text']
-        ):
+        text = data['text'].lower()
 
-            channel_id = data['channel']
-            thread_ts = data['ts']
+        if all (key in data for key in ('text', 'user')):
+
             user = data['user']
+
+            if 'novat' in data['text']:
+
+                response_message = f"""
+                Olá <@{user}>! Bem-vindo à Zanthus! :tada:
+
+                Sugiro que você comece por aqui:
+
+                1. Portal de Documentação da Zanthus: 
+                2. Como funciona o PDV da Zanthus?
+                3. Como funciona o workflow com o Jira?
+                4. Onde estão os códigos de lançamentos dos mísseis nucleares?
+                """
+
+            elif 'cafe' in data['text']:
+
+                response_message = f"""
+                Há café feito:
+
+                1. Por Máquina.
+                2. Pela Didi.
+                """
+
+            elif 'cotacoes' in data['text']:
+
+                cotacoes = 100
+
+                response_message = f"""
+                Na maioria dos casos, a cotação acaba sendo:
+
+                R$ {cotacoes} / PDV
+                """
+
+            # elif 'senha' in data['text']:
+
+                # Talvez criar algum atributo para mensagens privadas?
+
+            else:
+
+                response_message = f"""
+                Desculpe, mas eu não sei te responder! :persevere:
+                """
 
             web_client.chat_postMessage(
                 channel=channel_id,
-                text=f"Hi <@{user}>!",
+                text=response_message,
                 # thread_ts=thread_ts,
             )
