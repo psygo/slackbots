fb562e7a198d915287beb22ab966f8f63f857a31
philippe <philippefanaro@gmail.com>
2019-09-08 19:24:59 -0300

    fixes #3 and #5

--- zds_slack/slacky.py
+++ zds_slack/slacky.py
@@ -9,6 +9,7 @@ class CustomSlackBot():
     def __init__(
         self,
         slack_token,
+        path_to_responses='responses',
     ):
         """
         Initializes a custom Slack bot.
@@ -24,6 +25,8 @@ class CustomSlackBot():
 
         self.RTMClient = slack.RTMClient(token=slack_token)
 
+        # self.send_scheduled_msg()
+
     def start(self):
         """
         Starts the Client.
@@ -31,18 +34,37 @@ class CustomSlackBot():
 
         self.RTMClient.start()
 
+    def send_scheduled_msg(self):
+        """
+        Sends a scheduled message.
+        """
+
+        import datetime
+
+        now = datetime.datetime.now().timestamp()
+        schedule_time = now + 10
+        
+        self.RTMClient.chat_scheduleMessage(
+            channel='CN1JTR2Q7',
+            text='Hora do Bolo! :tada:',
+            post_at=schedule_time,
+        )
+
     @slack.RTMClient.run_on(event='message')
     def answer_message(**payload):
         """
         Answers a message sent to the channel.
         """
 
+        path_to_responses = 'responses'
+        msgs_handler = MsgsHandler(subfolder=path_to_responses)
+        msgs_dict = msgs_handler.msgs_dict
+
         data = payload['data']
         web_client = payload['web_client']
         rtm_client = payload['rtm_client']
         channel_id = data['channel']
         thread_ts = data['ts']
-
         text = data['text'].lower()
 
         if all (key in data for key in ('text', 'user')):
@@ -51,48 +73,129 @@ class CustomSlackBot():
 
             if 'novat' in data['text']:
 
-                response_message = f"""
-                Olá <@{user}>! Bem-vindo à Zanthus! :tada:
-
-                Sugiro que você comece por aqui:
-
-                1. Portal de Documentação da Zanthus: 
-                2. Como funciona o PDV da Zanthus?
-                3. Como funciona o workflow com o Jira?
-                4. Onde estão os códigos de lançamentos dos mísseis nucleares?
-                """
+                response_message = msgs_dict['novat'].replace(
+                    'user', 
+                    user,
+                )
 
             elif 'cafe' in data['text']:
 
-                response_message = f"""
-                Há café feito:
-
-                1. Por Máquina.
-                2. Pela Didi.
-                """
+                response_message = msgs_dict['cafe']
 
             elif 'cotacoes' in data['text']:
 
                 cotacoes = 100
 
-                response_message = f"""
-                Na maioria dos casos, a cotação acaba sendo:
+                response_message = msgs_dict['cotacoes'].replace(
+                    'cotacoes', 
+                    str(cotacoes),
+                )
 
-                R$ {cotacoes} / PDV
-                """
+            elif 'senha' in data['text']:
 
-            # elif 'senha' in data['text']:
-
-                # Talvez criar algum atributo para mensagens privadas?
+                response_message = msgs_dict['senha']
 
             else:
 
-                response_message = f"""
-                Desculpe, mas eu não sei te responder! :persevere:
-                """
+                response_message = msgs_dict['else']
 
-            web_client.chat_postMessage(
-                channel=channel_id,
+            post_message(
+                web_client=web_client,
+                channel=channel_id if 'senha' not in text else user,
                 text=response_message,
-                # thread_ts=thread_ts,
             )
+
+    @slack.RTMClient.run_on(event='message')
+    def send_scheduled_message(**payload):
+        """
+        Envia uma mensagem         
+        """
+
+        path_to_responses = 'scheduled'
+        msgs_handler = MsgsHandler(subfolder=path_to_responses)
+        msgs_dict = msgs_handler.msgs_dict
+
+        data = payload['data']
+        web_client = payload['web_client']
+        rtm_client = payload['rtm_client']
+        channel_id = data['channel']
+        thread_ts = data['ts']
+        text = data['text'].lower()
+
+        response_message = msgs_dict['bolo']
+
+        now = datetime.datetime.now().timestamp()
+        scheduled_time = now + 15
+
+        if all (key in data for key in ('text', 'user')):
+
+            user = data['user']
+
+            if 'senha' in text:
+
+                web_client.chat_scheduleMessage(
+                    channel=channel_id,
+                    post_at=scheduled_time,
+                    text=response_message,
+                )
+
+
+def post_message(
+    web_client,
+    channel,
+    text,
+):
+    """
+    POSTs a message with the Slack Client.
+    """
+
+    web_client.chat_postMessage(
+        channel=channel,
+        text=text,
+    )
+
+
+class MsgsHandler():
+    """
+    Manages the *response messages*.
+    """
+
+    def __init__(
+        self,
+        subfolder,  
+    ):
+        """
+        Initialization.
+        """
+
+        self.path_to_msgs = 'data/msgs'
+        self.subfolder = subfolder
+        self.complete_path_to_msgs = os.path.join(
+            self.path_to_msgs,
+            self.subfolder,
+        )
+
+        self.files = [
+            [dirpath, dirnames, filenames]
+            for dirpath, dirnames, filenames 
+            in os.walk(self.complete_path_to_msgs)
+        ]
+
+    @property
+    def msgs_dict(self):
+        """
+        Puts the messages into a dictionary.
+        """
+
+        msgs_dict = {}
+        for msg_file in self.files[0][2]:
+
+            path_to_msg_file = os.path.join(
+                self.complete_path_to_msgs, 
+                msg_file
+            )
+            msg_file_name_without_ext = os.path.splitext(msg_file)[0]
+            with open(path_to_msg_file, 'r') as f:
+                msgs_dict[msg_file_name_without_ext] = f.read()
+
+        return msgs_dict
